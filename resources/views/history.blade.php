a99af4ca5d010508179629d561a0fa0307b5034d
Simon <simon.nordstrom@chasacademy.se>
2020-01-27 16:01:21 +0100

    Add views for History

---
+++ resources/views/history.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+@section('content')
+    <div class="content">
+        <table>
+            <thead>
+                <tr>
+                    <th>Order nr</th>
+                    <th>Namn</th>
+                    <th>Klass</th>
+                    <th>Dryck</th>
+                    <th>Antal</th>
+                    <th>Pris</th>
+                </tr>
+                @foreach ($orders as $order)
+                    <tr>
+                    <td>{{order.ordernr}}</td>
+                    <td>{{order.name}}</td>
+                    <td>{{order.class}}</td>
+                    <td>{{order.drink}}</td>
+                    <td>{{order.amount}}</td>
+                    <td>{{order.price}}</td>
+                    </tr> 
+                @endforeach
+            </thead>
+        </table>
+    </div>
+    
+@endsection
