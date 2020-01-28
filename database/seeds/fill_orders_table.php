ae81455d471edd71e6d17c86bd14929e1711d7df
byHT <hung.ta79@outlook.com>
2020-01-28 12:25:46 +0100

    added seed family

--- database/seeds/fill_orders_table.php
+++ database/seeds/fill_orders_table.php
@@ -15,241 +15,209 @@ class fill_orders_table extends Seeder
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
         DB::table('orders')->insert([
             'orderID' => rand(),
             'drink_ID' => random_int(1,4),
             'student_ID' => random_int(1,4),
-            'date' => '2019-03-02 00:00:00',
-            'orderSum' => random_int(5,100)
+            'date' => '2019-03-02 00:00:00'
         ]);
 
     }
