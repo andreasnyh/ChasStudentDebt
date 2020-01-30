9b2619a579e261e865daa8755cf9f44cba06be62
vagrant <vagrant@homestead>
2020-01-30 10:39:35 +0000

    clean up drink controller class

--- app/Http/Controllers/DrinkController.php
+++ app/Http/Controllers/DrinkController.php
@@ -13,32 +13,11 @@ class DrinkController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        //return Drink::all();
-        
-        //return view('show_drinks');
-
-        
-        //$drinks = DB::table('drinks')->select('name', 'cost', 'stock')->get();
-        
-        //return view('show_drinks')->with('drinks', $drinks);
-
-        //$drinks =  Drink::where('name', 'Moonshine')->get();
-        //return view('show_drinks', ['drinks'=>$drinks]);
-
-        //echo "hello 1";
         $drinks =  Drink::get();
-        //DB::table('x')->get();
-        //echo "hello";
-        //var_dump($drinks);
-        //dd($drinks);
+        
         return view('show_drinks', [
             'drinks' => $drinks
         ]);
-        
-        
-
-        //$drinks=CLSDB::table('drinks')->get();
-        //print_r($drinks);
         
     }
 }
