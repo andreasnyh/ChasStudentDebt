b3fa26208126ff73131d146663ac7e3a92e21d67
Simon <simon.nordstrom@chasacademy.se>
2020-01-30 23:58:06 +0100

    Add typography

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,9 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Fredoka+One|Luckiest+Guy|Orbitron|Russo+One|Squada+One&display=swap" rel="stylesheet">
     <title>Chas Bar Tab</title>
 </head>
-
 <body>
     <nav class=" text-light bg-dark">
         <img class="pl-4 py-4" src="https://chas.se/wp-content/uploads/2017/03/ChasLogoSvgWht.svg" width="200"
@@ -20,16 +20,23 @@
     <div class="container">
         @yield('content')
     </div>
-    <footer class="bg-dark text-light mt-5 p-4">
-        <h3>About us</h3>
-        <h6> <a href="https://github.com/SimonNord">Simon Nordström</a></h6>
-        <h6> <a href="https://github.com/andreasnyh">Andréas Ny</a></h6>
-        <h6> <a href="https://github.com/ByHT">Hung Ta</a></h6>
-        <h6> <a href="https://github.com/erax83">Erik Axelsson</a></h6>
-        <h6> <a href="https://github.com/DylanNore">Dylan Nore</a></h6>
-        <h6> <a href="https://github.com/Voldakka">Karl Falk</a></h6>
-        <h6><a href="https://github.com/andreasnyh/ChasStudentDebt/">Project on Github</a></h6>
+    <footer class=" d-flex flex-row justify-content-between bg-dark text-light mt-5 p-4">
+        <section class="d-flex flex-column justify-content-between">
+        <a href="https://github.com/SimonNord">Simon Nordström</a>
+        <a href="https://github.com/andreasnyh">Andréas Ny</a>
+    </section>
+    <section class="d-flex flex-column justify-content-between">
+        <a href="https://github.com/ByHT">Hung Ta</a>
+        <a href="https://github.com/erax83">Erik Axelsson</a>
+    </section>
+    <section class="d-flex flex-column justify-content-between">
+        <a href="https://github.com/DylanNore">Dylan Nore</a>
+        <a href="https://github.com/Voldakka">Karl Falk</a>
+    </section>
+    <section class="d-flex flex-column justify-content-between">
+        <a href="https://github.com/andreasnyh/ChasStudentDebt/">Project on Github</a>
         <p>&copy; 2020 Chas Academy</p>
+    </section>
     </footer>
     <script src="/js/main.js"></script>
 </body>
