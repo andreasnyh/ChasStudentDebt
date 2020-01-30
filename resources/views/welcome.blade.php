4314e78df5160d935fa62f929ae193d9809bc5ee
Simon <simon.nordstrom@chasacademy.se>
2020-01-31 00:03:19 +0100

    small change - note position

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,10 @@
         <h3 class="h3 sub-heading mb-4"><span class="dark-text"><strong>Chas Bar Tab</strong></span> registrerar enkelt dina dryckesutlägg under Chas Academy´s förstklassiga event.
                        "Men notan då?" - Den betalar du direkt, senare eller lite då och då<span class="dark-text">*</span></h3>
         <ul class="list-group list-group-horizontal">
-            <li class="list-group-item"><a href="/students">Lista Studenter</a></li>
+            <li class="list-group-item"><a href="/students">Studenter</a></li>
             <li class="list-group-item"><a href="/order">Ny beställning</a></li>
             <li class="list-group-item"><a href="/drinks">Drycker</a></li>
             <li class="list-group-item"><a href="/history">Historia</a></li>
-            <li class="list-group-item"><a href="https://github.com/andreasnyh/ChasStudentDebt/">GitHub</a></li>
         </ul>
         <p class="note">*<span class=uppercase><em>innan</span> examensdagen freeloaders</em></p>
     </div>
