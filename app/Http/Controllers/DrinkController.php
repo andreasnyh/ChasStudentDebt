71c6b27d8205f5e1e43a8c4c313bb3233592e52a
vagrant <vagrant@homestead>
2020-01-30 10:24:49 +0000

    Add drinks list

--- app/Http/Controllers/DrinkController.php
+++ app/Http/Controllers/DrinkController.php
@@ -3,10 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Drink;
 
 class DrinkController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index() {
-        return view('show_drinks');
+        //return Drink::all();
+        
+        //return view('show_drinks');
+
+        
+        //$drinks = DB::table('drinks')->select('name', 'cost', 'stock')->get();
+        
+        //return view('show_drinks')->with('drinks', $drinks);
+
+        //$drinks =  Drink::where('name', 'Moonshine')->get();
+        //return view('show_drinks', ['drinks'=>$drinks]);
+
+        //echo "hello 1";
+        $drinks =  Drink::get();
+        //DB::table('x')->get();
+        //echo "hello";
+        //var_dump($drinks);
+        //dd($drinks);
+        return view('show_drinks', [
+            'drinks' => $drinks
+        ]);
+        
+        
+
+        //$drinks=CLSDB::table('drinks')->get();
+        //print_r($drinks);
+        
     }
 }
--- resources/views/show_drinks.blade.php
+++ resources/views/show_drinks.blade.php
@@ -1,16 +1,25 @@
 @extends('layout')
     @section('content')
     <table>
-        <tr>
-            <th><h3>Namn</h3></th>
-            <th><h3>Antal</h3></th>
-            <th><h3>Pris</h3></th>
-        </tr>
-        <tr>
-            <td>Öl</td>
-            <td>4</td>
-            <td>20</td>
-        </tr>
+        <thead>
+            <tr>
+                <th><h3>Namn</h3></th>
+                <th><h3>Antal</h3></th>
+                <th><h3>Pris</h3></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($drinks as $drink)
+                <tr>
+                    <td>{{$drink->name}}</td>
+                    <td>{{$drink->stock}}</td>
+                    <td>{{$drink->cost}}:-</td>
+                </tr> 
+            @endforeach
+           
+        </tbody>
+        
+       
     </table>
     <form method="get" action="/">
         <input type="submit" value="Lägg Till"> 
