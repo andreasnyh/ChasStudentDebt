c3b6bbb5e33f52122e8cac4a1242fb55cc636c9f
Andreas Ny <andreas.ny@chasacademy.se>
2020-02-12 14:38:42 +0100

    update weather

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,21 @@
 
 @section('content')
     <?php
-    $api = json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=054591b3363fee277db526c645d8295d'), true);
-    $temp = $api['main']['temp'] - 273.15; // Subtract from kelvin value to get celsius
+        $apiKey = env('API_KEY');
+        $city = "Stockholm";
+        $api = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$apiKey}"), true);
+        $temp =  round($api['main']['temp'] - 273.15, 1); // Subtract from kelvin value to get celsius
+        $tempFeels = round($api['main']['feels_like'] - 273.15, 1); // Subtract from kelvin value to get celsius
+        $weather = $api['weather'];
 
-    echo ('<div class="text-center align-items-center justify-content-center">' .
-        "<h4>The temperature in Stockholm is {$temp} degrees with {$api['weather'][0]['description']}.</h4></div>");
 
+
+        echo('<div class="text-center align-items-center justify-content-center">' .
+            "<h4>The temperature in {$city} is {$temp}℃, feels like {$tempFeels}℃ with {$api['weather'][0]['description']}.</h4>");
+            foreach ($weather as $wType){
+                echo("<img src='http://openweathermap.org/img/wn/{$wType['icon']}@2x.png'>");
+            }
+        echo("</div>");
     ?>
     <div class="content text-center d-flex flex-column align-items-center flex-d-col justify-content-center">
         <h1 class="mb-4 h1 main-heading">Vi kommer ihåg åt dig</h1>
